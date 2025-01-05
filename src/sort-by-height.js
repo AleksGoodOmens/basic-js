ddcb8382cdff652ac79d853b0817f4a476237c10
Aleks Gomeniuk <agsfinks@gmail.com>
2025-01-05 20:34:29 +0000

    feat: sortByHeight reduce filter sort forEach splice

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+	const negativeIndex = arr.reduce((acc, n, i) => {
+		if (n === -1) {
+			acc.push(i);
+		}
+		return acc;
+	}, []);
+
+	const sortedPos = arr.filter((n) => n !== -1).sort((a, b) => a - b);
+
+	negativeIndex.forEach((i) => {
+		sortedPos.splice(i, 0, -1);
+	});
+	return sortedPos;
 }
 
 module.exports = {
-  sortByHeight
+	sortByHeight,
 };
